878642b9d70ffcfd00e7984677e9de8131e06135
arsalan4work <m.arsalan4210@gmail.com>
2025-07-16 13:15:00 +0500

    add files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <h1>Hello from Pizza Bites</h1>
+      <Button><Link href="/login">Login</Link></Button>
     </div>
   );
 }
